2766397e708babf15a40634bd5678ca7cd2eb7d3
adlan100 <manarata345@gmail.com>
2025-02-23 13:29:00 +0500

    Add files via upload

--- Game/games/models.py
+++ Game/games/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255,
+                            verbose_name = "Название игры")
+    image = models.ImageField(upload_to='game_images',
+                              default='default.jpg',
+                              verbose_name="Изображеине игры")
+    slug = models.SlugField(max_length=200,
+                            unique=True,
+                            blank=True,
+                            verbose_name="URL-метка")
+
+def __str__(self):
+    return self.title
+
+def save(self, *args, **kwargs):
+    if not self.slug:
+        self.slug = slugify(self.title)
+        super(Game, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
--- Game/games/views.py
+++ Game/games/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+GAMES = [
+    "Portal 2",
+    "Minecraft",
+    "Half-life 2",
+    "Spore",
+    "Buckshot roullet"
+]
 
 def game_list(request):
-    return render(request, 'games/game_list.html')
+    query = request.GET.get('q')
+    if query:
+        filtered_games = [game for game in GAMES if query.lower() in game.lower()]
+    else:
+        filtered_games = GAMES
+
+    context = {'games': filtered_games}
+    return render(request, 'games/game_list.html', context)
